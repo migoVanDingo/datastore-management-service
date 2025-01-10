2ff673ec6198360cfc445ba73bb8b09f3a80cc5e
migo <migo.vandingo@gmail.com>
2025-01-09 22:10:07 -0700

    Migrate aolme_videos 1-8786

--- api/utility/utility_api.py
+++ api/utility/utility_api.py
@@ -15,9 +15,8 @@ utility_api = Blueprint('utility_api', __name__)
 def health():
     try:
         dao_request = Request()
-        add_videos_response = dao_request.read_all(request_id="READ_ALL", query="SELECT * FROM aolme_videos WHERE set_id IS NULL AND idx BETWEEN 8950 AND 9353")
+        add_videos_response = dao_request.read_all(request_id="READ_ALL", query="SELECT * FROM aolme_videos WHERE set_id IS NULL AND idx BETWEEN 6001 AND 7000")
         #8786
-        #8938
         current_app.logger.info(f"Response len: {len(add_videos_response['response'])}")
 
         if not add_videos_response["response"]:
@@ -34,7 +33,7 @@ def health():
         # For each video,
         for video in videos:
 
-            
+            new_request = Request()
             if count % 100 == 0:
                 print("Sleeping for 10 second...")
                 time.sleep(1)
@@ -52,43 +51,39 @@ def health():
 
             # If element not in dictionary add it as key and generate q2 and q3 subfields.
             current_set_id = "" 
-
-            name_2 = name_split[2].replace(".mp4", "")
             
-            # For first 8786
-            #new_index = name_split[0] + "_" + name_split[1]
-            new_index = name_split[0] + "_" + name_2
+            new_index = name_split[0] + "_" + name_split[1]
             if len(name_split) >= 2:
                 
                 if new_index not in group_dict:
                     # Check if second element is q2 or q3, if value is null generate set_id and update the video record with the set_id
                     current_set_id = generate_id()
-                    if name_2 == "q2":
+                    if name_split[1] == "q2":
                         group_dict[new_index] = {"q2": current_set_id, "q3": None}
-                    elif name_2 == "q3":
+                    elif name_split[1] == "q3":
                         group_dict[new_index] = {"q2": None, "q3": current_set_id}
                 else:
                     # Else get the set_id and update the video record with the set_id
-                    current_set_id = group_dict[new_index]["q2"] if name_2 == "q2" else group_dict[new_index]["q3"]
+                    current_set_id = group_dict[new_index]["q2"] if name_split[1] == "q2" else group_dict[new_index]["q3"]
             else: 
                 print(f"NAME_SPLIT_WEIRD -- Skipping video: {video_name}")
                 continue
 
-            video_name = name_split[0] + "_" + name_2 + "_" + name_split[1] + ".mp4"
+            
             
             # Update set_name with first element
-            dao_request.update("UPDATE_AOLME_VIDEOS", "aolme_videos", "video_id", video["video_id"], {"set_id": current_set_id, "set_name": new_index, "video_name": video_name})
+            new_request.update("UPDATE_AOLME_VIDEOS", "aolme_videos", "video_id", video["video_id"], {"set_id": current_set_id, "set_name": new_index})
 
             # Then create a files record payload
             # {datastore_id, file_path="metadata", file_name, file_type="video", file_size="null", create_method="LINK", created_by="DEVROOT", metadata={video_id="video_id from video table", set_id="set_id from video table", url="link from video table", cohort, level, school, quality, date, facilitator, group_name} }
 
             current_in_set = None
             total_in_set = None
-            if len(name_split) > 2 and "-" in name_split[1]:
-                set_num_split = name_split[1].split("-")
+            if len(name_split) > 2 and "-" in name_split[2]:
+                set_num_split = name_split[2].split("-")
             
 
-                total_in_set = set_num_split[1]
+                total_in_set = set_num_split[1].replace(".mp4", "")
                 current_in_set = set_num_split[0]
                 #remove leading zero from current_in_set
                 current_in_set = current_in_set.lstrip("0")
@@ -122,7 +117,7 @@ def health():
 
             }
             
-            insert_response = dao_request.insert("INSERT_AOLME_FILES", Constant.table["FILES"], data)
+            insert_response = new_request.insert("INSERT_AOLME_FILES", Constant.table["FILES"], data)
             if not insert_response["response"]:
                 raise Exception(f"Failed to insert record for video: {video_name}")
 
